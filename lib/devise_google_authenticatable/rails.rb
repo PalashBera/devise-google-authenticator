ad39c08ea1bfea46fb834f6b83c0a91c372f401c
Palash Bera <palashbera1234@gmail.com>
2020-11-06 19:41:05 +0530

    Redirecting to GA authenticate path using warden after_authentication method

--- lib/devise_google_authenticatable/rails.rb
+++ lib/devise_google_authenticatable/rails.rb
@@ -1,8 +1,11 @@
 module DeviseGoogleAuthenticator
   class Engine < ::Rails::Engine # :nodoc:
-    ActionDispatch::Callbacks.to_prepare do
+    ActiveSupport::Reloader.to_prepare do
       DeviseGoogleAuthenticator::Patches.apply
     end
 
+    ActiveSupport.on_load(:action_controller) do
+      include DeviseGoogleAuthenticator::Controllers::Helpers
+    end
   end
 end
